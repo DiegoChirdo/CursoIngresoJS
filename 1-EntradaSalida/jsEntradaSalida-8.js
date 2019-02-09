95b10447dee60c80d5243dbbd632aaeea5fe733a
Chirdo <wchirdo@gmail.com>
2019-02-08 21:47:24 -0300

    Ejercicio 8 terminado

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,22 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var dividendo;
+	var divisor;
+	var division;
+
+
+	dividendo=numeroDividendo.value;
+	divisor=numeroDivisor.value;
+
+	dividendo=parseInt(dividendo);
+	divisor=parseInt(divisor);
+
+	// % = mòdulo (NO PORCENTAJE NI DIVISIÓ) El ejercicio no es de divisiòn sino para calcular el resto
+
+
+	division=dividendo%divisor;
+	alert("Resultado es "+division);
+
+
 }
