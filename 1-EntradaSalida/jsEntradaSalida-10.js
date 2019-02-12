3965e12a21f2e2bffe41373b71ecc49667a3481f
DiegoChirdo <wchirdo@gmail.com>
2019-02-12 00:21:41 -0300

    Ejercicios 1-10 hechos

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,22 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var sueldo;
+	var descuento;
+	var resultado;
+
+	sueldo = document.getElementById('importe').value;
+	sueldo = parseInt(sueldo);
+
+	descuento=parseInt(descuento);
+	descuento=sueldo * 25 / 100;
+
+	resultado=document.getElementById('resultado').value;
+	resultado=parseInt(resultado);
+	resultado= sueldo-descuento;
+
+	document.getElementById('resultado').value = resultado;
+
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,41 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
 	
+	var sueldo;
+	var Aumento;
+	var resultado;
+
+	sueldo = document.getElementById('sueldo').value;
+	sueldo = parseInt(sueldo);
+	
+	Aumento = parseInt(Aumento)
+	Aumento = sueldo * 10 / 100;
+
+	resultado = document.getElementById('resultado').value;
+	resultado = parseInt(resultado);
+	resultado = Aumento + sueldo;
+
+	document.getElementById('resultado').value = resultado;
+
+
+
+	//var misueldo;
+	//var aumento;
+	//var final;
+
+	//misueldo = document.getElementByID('sueldo').value
+	//misueldo=parseInt(misueldo);
+
+	//aumento = parseInt(aumento);
+	//aumento = misueldo * 10 /100;
+
+	//final = document.getElementByID('resultado').value;
+	//final = parseInt(resultado);
+	//final = aumento + misueldo;
+
+	//document.getElementByID('resultado').value = final;
+
+
+
 }
