122d12951a3dfd39d081178011f0a67d8c579eef
Diego <wchirdo@gmail.com>
2019-02-14 20:07:35 -0300

    Ejercicios terminadooos

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
+	var cuenta;
+	var amigos;
+	var iva;
+	var total;
+	var propina;
+	var resultado;
+	var pago;
+
+	cuenta = cuentatotal.value;
+	amigos = cantidaddeamigos.value;
+
+	cuenta = parseInt(cuenta);
+	amigos = parseInt(amigos);
+
+	iva = cuenta * 21 / 100;
+	total = cuenta + iva;
+	propina = total * 10 / 100;
+	resultado = total + propina;
+	pago = (total + propina) / amigos;
+
+	alert("La cuenta es de $"+cuenta+", sumándose el IVA (%21), serían en total, $"+total+". En caso de contar una propina, al ser el %10, esta sería de $"+propina+". Lo que daría un resultado de $"+resultado+". Dado que ustedes son "+amigos+", deberían pagar "+pago+" cada uno.)";
+		
+	
+	
+
+
+
 
 }
