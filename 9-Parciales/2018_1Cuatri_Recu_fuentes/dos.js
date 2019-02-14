1b665a86d294465e5f8ccb7bb591bd4341b7da93
Diego <wchirdo@gmail.com>
2019-02-14 19:54:11 -0300

    En proceso

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
-  
+
+	var compra;
+	var descuento;
+	var iva;
+	
+
+	compra = gasto.value;
+	compra = parseInt(compra);
+
+	descuento = compra * 10 / 100 + compra;
+	iva = descuento - (descuento * 21 / 100); 
+
+
+	alert("tu compra es de $"+compra", tenes un descuento del 10% queda en $"+descuento+", mas el iva es $"+iva+"");
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,23 @@
 
 function mostrar()
 {
+	var mascota1;
+	var mascota2;
+	var pesomascota1;
+	var pesomascota2;
+	var suma;
 
+	mascota1=mascota1.value;
+	mascota2=mascota2.value;
+
+	pesomascota1=pesomascota1.value;
+	pesomascota2=pesomascota2.value;
+
+	pesomascota2=parseInt(pesomascota1);
+	pesomascota1=parseInt(pesomascota2);
+
+	suma = pesomascota1 + pesomascota2;
+
+
+	alert("tenes dos mascotas "+mascota1+" y "+mascota2+", que pesan"+pesomascota1+" y "+pesomascota2+" kilos, la suma de los kilos es "+suma"");
 }
