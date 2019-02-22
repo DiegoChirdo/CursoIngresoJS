a57aacd35a19a28ef9e2d69d2adea2c43348063f
Diego Chirdo <wchirdo@gmail.com>
2019-02-21 21:17:27 -0300

    Ejercicio por hacer

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -13,12 +13,46 @@
 // paquetes no tiene descuento adicional 
 
 
+
 function mostrar()
 {
 
+	var precio;
+	var descuento;
+	var descuento2;
+	var modopago;
+	var paquete;
+	var paquete;
+
+	precio=prompt("Precio de la habitación");
+	precio=parseInt(precio);
+	modopago=prompt("¿Cómo paga? (Efectivo, Visa, Paypal, Mercadopago, Otro");
+	paquete=prompt("¿Qué paquete quiere? (TodoIncluido, SoloDesayunos)");
+
+	switch (modopago) 
+	{
+		case "Visa":
+		descuento = (precio * 10 / 100);
+		break;
+		case "PayPal":
+		descuento = (precio * 15 / 100);
+		break;
+		case "MercadoPago":
+		descuento = (precio * 10 / 100);
+		break;
+		case "Efectivo":
+		descuento = (precio * 20 / 100);
+		break;
+		default:
+		descuento = (precio * 5 / 100);
+		break;
+	}	
+
+	switch (paquete) && modopago = "PayPal"
+	{
+		case "TodoIncluido":
+		descuento2 = (precio * 10 / 100);
 
 
-
-
-
+	}
 }
